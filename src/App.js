9dfe38ef9f2bb9c291312a569c24b35c5493bc77
Raviteja Gandam <gandamraviteja@gmail.com>
2024-05-14 13:04:55 +0530

    Chatfeed Added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/ChatFeed';
 
 function App() {
   return (
     <div className="App">
       <ChatEngine
         height="100vh"
-        projectID=""
-        userName=" "
-        userSecret=" "
+        projectID="
+        bfc29f42-0229-4323-9a37-fea330ef4a4e"
+        userName="Raviteja"
+        userSecret="1234"
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+
       />
     </div>
   );
